254fc7f6522050cae1083f942f766ac79f17ffd4
nurzat87 <zusubalievanurzat@gmail.com>
2021-01-25 23:18:19 +0300

    added todo marking

--- todo/urls.py
+++ todo/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     path("test3/", third),
     path("add-todo/", add_todo, name="add-todo"),
     path("delete-todo/<id>/", delete_todo, name="delete-todo"),
+    path("mark-todo/<id>/", mark_todo, name="mark-todo"),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
